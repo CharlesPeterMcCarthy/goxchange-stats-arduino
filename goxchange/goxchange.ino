8527be60ffe9d059e61787d0f7b1669c0b78b305
CharlesPeterMcCarthy <charles@studybundles.com>
2019-05-24 19:59:06 +0100

    Sound On Button & Flash Screen Alert

--- goxchange/goxchange.ino
+++ goxchange/goxchange.ino
@@ -3,9 +3,14 @@
 #include <HttpClient.h>
 #include <Process.h>
 #include <ArduinoJson.h>
+#include <Time.h>
+#include <TimeLib.h>
 
 boolean isStartup;
+boolean soundOn = true;
 int currentUserCount = 0;
+const byte BUTTON_PIN = 8;
+const byte SOUND_PIN = 6;
 
 rgb_lcd lcd;
 HttpClient http;
@@ -16,15 +21,56 @@ void setup() {
   Serial.begin(9600);
   Bridge.begin();
 
-  pinMode(6, OUTPUT);
+  pinMode(SOUND_PIN, OUTPUT);
 
   delay(1000);
 
-  setupLCD();
-  clearLCD();
+  SetupLCD();
+  ClearLCD();
 }
 
 void loop() {
+  checkButtonValue();
+
+  long curTime = now();
+  if (curTime % 10 == 0) CallAPI();
+    
+  delay(1);
+}
+
+void SetupLCD() {
+  lcd.begin(16,2);
+  DefaultColorLCD();
+  lcd.print("Starting Up...");
+  delay(2000);
+}
+
+void DefaultColorLCD() {
+  lcd.setRGB(39, 168, 173);
+}
+
+void ClearLCD() {
+  lcd.clear();
+}
+
+void checkButtonValue() {
+  int btnVal = digitalRead(BUTTON_PIN);
+
+  if (btnVal == 1) {                        // Button has been pressed
+    Serial.println("Button On");
+    while (digitalRead(BUTTON_PIN) == 1);   // Wait until button is not being pressed
+
+    soundOn = !soundOn;                 // Sound on / off
+    ClearLCD();
+    lcd.setCursor(0, 0);
+    lcd.print(soundOn ? "Sound is on" : "Sound is off");
+    delay(1000);
+    PrintCurrentCount();
+  }
+}
+
+void CallAPI() {
+  Serial.println("Call");
   StaticJsonBuffer<200> jsonBuffer;
   String response = "";
 
@@ -36,8 +82,8 @@ void loop() {
   linux.addParameter("https://next.goxchange.io/api/usercount");
   linux.runAsynchronously();
   
-  while (linux.running());          // Wait for script to run
-  while (linux.available()) {       // Get response
+  while (linux.running());
+  while (linux.available()) {
     response += linux.readString();
   }
 
@@ -48,41 +94,49 @@ void loop() {
   SortInformation(json);
   
   if (isStartup) isStartup = false;
-  
-  delay(10000);
-}
-
-void setupLCD() {
-  lcd.begin(16,2);                // 2 rows, 16 columns
-  lcd.setRGB(24, 167, 219);       // Set blue background
-  lcd.print("Starting Up...");
-  delay(2000);
-}
-
-void clearLCD() {
-  lcd.clear();
 }
 
 void SortInformation(JsonObject& info) {
-  clearLCD();
   int updatedUserCount = info["users"];
-  Serial.println(isStartup);
+
   if (!isStartup && updatedUserCount > currentUserCount) {
-    currentUserCount = updatedUserCount;
+    int diff = updatedUserCount - currentUserCount;
     
-    PlaySound();
+    if (soundOn) SoundAlert(diff);
+    FlashScreen();
   }
-  
+
   currentUserCount = updatedUserCount;
   
+  PrintCurrentCount();
+}
+
+void PrintCurrentCount() {
+  ClearLCD();
   lcd.setCursor(0, 0);
   lcd.print("User Count: ");
-  //lcd.setCursor(0, 1);
   lcd.print(currentUserCount);
 }
 
+void SoundAlert(int diff) {
+  for (int i = 0; i < diff; i++) {
+    PlaySound();
+    delay(500);
+  }  
+}
+
 void PlaySound() {
-  digitalWrite(6, HIGH);
+  digitalWrite(SOUND_PIN, HIGH);
   delay(10);
-  digitalWrite(6, LOW);
+  digitalWrite(SOUND_PIN, LOW);
+}
+
+void FlashScreen() {
+  for (int i = 0; i < 10; i++) {
+    lcd.setRGB(21, 255, 0); 
+    delay(100);
+    lcd.setRGB(255, 0, 63); 
+    delay(100);
+  }
+  lcd.setRGB(39, 168, 173);
 }
